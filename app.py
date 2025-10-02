a82654e1ef19c3ff6c33eb7ac27de75e3e0134d2
Gusts Linkevics <gusts.linkevics@rtu.lv>
2025-10-02 09:33:27 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("home.html")
+
+
+@app.route('/orders')
+def order():
+    return render_template("orders.html", number_of_orders=3, orders=["121234", "345643563", "346345634r56"])
+
+
+@app.route('/orders/<orderid>')
+def order_by_id(orderid):
+    return render_template("order_details.html", order_id=orderid)
+
+
+@app.route('/<name>')
+def user(name):
+    return '<h1>Name is:' + name + '</h1>'
+
+
+@app.route('/admin')
+def admin():
+    # return redirect(url_for("hello_world"))
+    return redirect(url_for("user", name="Admin2"))
+
+
+if __name__ == '__main__':
+    app.run()
