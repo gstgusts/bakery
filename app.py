fcb20df69499cddd4406d8bd8bdb4f0d0fc32c9f
Gusts Linkevics <gusts.linkevics@rtu.lv>
2025-10-06 15:51:39 +0300

    Profile page added and login

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template("home.html")
+def home():  # put application's code here
+    return render_template('home.html')
 
 
 @app.route('/orders')
 def order():
-    return render_template("orders.html", number_of_orders=3, orders=["121234", "345643563", "346345634r56"])
+    return render_template('orders.html', number_of_orders=3, orders=['121234', '345643563', '346345634r56'])
 
 
 @app.route('/orders/<orderid>')
 def order_by_id(orderid):
-    return render_template("order_details.html", order_id=orderid)
+    return render_template('order_details.html', order_id=orderid)
 
 
 @app.route('/<name>')
@@ -25,9 +25,28 @@ def user(name):
 
 @app.route('/admin')
 def admin():
-    # return redirect(url_for("hello_world"))
-    return redirect(url_for("user", name="Admin2"))
+    # return redirect(url_for('hello_world'))
+    return redirect(url_for('user', name='Admin2'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['email']
+        password = request.form['pwd']
+
+        if password != 'abc123':
+            return redirect(url_for('login'))
+
+        return render_template('profile.html', user=username)
+    else:
+        return render_template('login.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
